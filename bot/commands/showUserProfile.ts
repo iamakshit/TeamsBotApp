e3719dd69667e9cbea2a0d588c3cfaad9f21af67
Akshit Gupta <akshitonline@gmail.com>
2022-09-22 11:19:42 +0530

    Changes in the userProfile

--- bot/commands/showUserProfile.ts
+++ bot/commands/showUserProfile.ts
@@ -19,15 +19,18 @@ export class ShowUserProfile extends SSOCommand {
     // Call Microsoft Graph half of user
     const teamsfx = new TeamsFx().setSsoToken(ssoToken);
     const graphClient = createMicrosoftGraphClient(teamsfx, [
-      "User.Read",
+      "User.Read"
     ]);
-    const me = await graphClient.api("/me").get();
+   // const me = await graphClient.api("/me").get();
+   const me = await graphClient.api('/me').get();
     if (me) {
       await context.sendActivity(
         `You're logged in as ${me.displayName} (${me.userPrincipalName})${
           me.jobTitle ? `; your job title is: ${me.jobTitle}` : ""
         }.`
       );
+
+      
 
       // show user picture
       let photoBinary: ArrayBuffer;
@@ -42,15 +45,51 @@ export class ShowUserProfile extends SSOCommand {
 
       const buffer = Buffer.from(photoBinary);
       const imageUri = "data:image/png;base64," + buffer.toString("base64");
-      const card = CardFactory.thumbnailCard(
-        "User Picture",
-        CardFactory.images([imageUri])
-      );
-      await context.sendActivity({ attachments: [card] });
+    /*  const card = CardFactory.thumbnailCard(
+        "Akshit",
+        "Senior Software Engineer \n <p></p> akshitgupta@microsoft.com",
+        null
+      );*/
+    //  const card = CardFactory.heroCard("Akshit Gupta","akshitgupta@microsoft.com");
+   
+      const card1 = CardFactory.heroCard("Prashant Tripathi",null,null, {
+        text: "IP and Comp IDC Substrate 1190 <p></p> HYD SOHINI/Mobile <p></p><p></p>"+
+        "His interests are as follows:"+
+        "<ul>"+
+        "<li>Substrate</li>"+
+        "<li>Accomdation</li>"+
+        "<li>Onboarding</li>"+
+        "</ul>"
+      });
+
+      const card2 = CardFactory.heroCard("Harshikesh Kumar",null,null, {
+        text: "IP and Comp IDC Substrate 1190 <p></p> HYD SOHINI/Mobile <p></p><p></p>"+
+        "His interests are as follows:"+
+        "<ul>"+
+        "<li>Substrate</li>"+
+        "<li>Mentorship</li>"+
+        "<li>Tech</li>"+
+        "</ul>"
+      });
+
+      const card3 = CardFactory.heroCard("Soumyajit Pal",null,null, {
+        text: "IP and Comp IDC Substrate 1190 <p></p> HYD SOHINI/Mobile <p></p><p></p>"+
+        "His interests are as follows:"+
+        "<ul>"+
+        "<li>Fun</li>"+
+        "<li>Mentorship</li>"+
+        "<li>Substrate</li>"+
+        "</ul>"
+      });
+
+
+      await context.sendActivity({ attachments: [card1, card2, card3] });
     } else {
       await context.sendActivity(
         "Could not retrieve profile information from Microsoft Graph."
       );
     }
+
+
   }
 }
